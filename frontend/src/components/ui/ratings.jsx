42c4e4dafb6732722c485689abc48728b1b5c9a5
Ifeouwa Olujobi <ifeoluwaolujobi@gmail.com>
2025-08-16 20:33:02 +0100

    Dashboard and profile page

---
+++ frontend/src/components/ui/ratings.jsx
@@ -0,0 +1,9 @@
+function ratings({ children }) {
+  return <div className='flex items-center'>{children}</div>;
+  // <p>5</p>
+  //     <Star />
+  //     <Progress value={11} />
+  //     <p>11</p>
+}
+
+export default ratings;
